deeac25fb2dbbf5f511a6dbfaf4b2471c5dd156c
marvin4keap <marvin.abisrror@keap.com>
2022-01-04 18:31:09 -0500

    0003 - app component, axios

--- src/App.js
+++ src/App.js
@@ -1,12 +1,49 @@
 import React, {Component} from 'react';
+import axios from 'axios'
+
+const axiosGitHubGraphQL = axios.create({
+  baseURL: 'https://api.github.com/graphql',
+  headers: {
+    Authorization: `bearer ${process.env.YOUR_GITHUB_PERSONAL_ACCESS_TOKEN}`,
+  }
+});
 
 const TITLE = 'React GraphQL GitHub Client';
 
 class App extends Component {
+  state = {
+    path: 'the-road-to-learn-react/the-road-to-learn-react',
+  };
+  componentDidMount() {
+    //fetch data
+  }
+  onChange = event => {
+    this.setState({
+      path: event.target.value
+    });
+  };
+  onSubmit = event => {
+    //fetch data
+    event.preventDefault();
+  }
   render() {
     return (
       <div>
         <h1>{TITLE}</h1>
+        <form onSubmit={this.onSubmit}>
+          <label htmlFor='url'>
+            Show open issues for https://github.com/
+          </label>
+          <input
+            id='url'
+            type='text'
+            value={path}
+            onChange={this.onChange}
+            style={{width: '300px'}}
+          />
+          <button type='submit'>Search</button>
+        </form>
+        <hr/>
       </div>
     )
   }
